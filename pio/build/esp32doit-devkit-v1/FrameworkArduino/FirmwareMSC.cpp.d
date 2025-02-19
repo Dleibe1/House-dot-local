5ebfd14d597ff417e0758780c9910ed0a9723efb
dleibe1 <daniel.leibensperger28@gmail.com>
2025-02-19 18:40:39 -0500

    works

--- pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.o: \
- /Users/danielleibensperger/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- /Users/danielleibensperger/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- /Users/danielleibensperger/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- /Users/danielleibensperger/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- pio/build/esp32doit-devkit-v1/libf69/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/libf69/WebServer/detail/mimetable.cpp.o: \
- /Users/danielleibensperger/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- /Users/danielleibensperger/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- /Users/danielleibensperger/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
