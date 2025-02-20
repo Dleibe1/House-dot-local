a64ad65e611dfc477e1e4c806d501145212ab590
dleibe1 <daniel.leibensperger28@gmail.com>
2025-02-19 22:18:36 -0500

    removed wifi credentials

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,24 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
-#include <LittleFS.h>  // Change from SPIFFS.h to LittleFS.h
+#include <ESPmDNS.h>
+#include <LittleFS.h> // Change from SPIFFS.h to LittleFS.h
 
-const char* ssid = "kutzeberts";
-const char* password = "ebertskutz";
+const char *ssid = "";
+const char *password = "";
 
 AsyncWebServer server(80);
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
+    WiFi.setHostname("matt");
 
     // Connect to WiFi
     WiFi.begin(ssid, password);
     Serial.print("Connecting to WiFi...");
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
         delay(1000);
         Serial.print(".");
     }
@@ -24,8 +28,17 @@ void setup() {
     Serial.print("ESP32 IP Address: ");
     Serial.println(WiFi.localIP());
 
+    if (!MDNS.begin("matt"))
+    {
+        Serial.println("Error starting mDNS responder!");
+        while (1)
+        {
+            delay(1000);
+        }
+    }
     // Initialize LittleFS instead of SPIFFS
-    if (!LittleFS.begin(true)) {
+    if (!LittleFS.begin(true))
+    {
         Serial.println("LittleFS initialization failed!");
         return;
     }
@@ -34,22 +47,20 @@ void setup() {
     File root = LittleFS.open("/");
     File file = root.openNextFile();
 
-    while (file) {
+    while (file)
+    {
         Serial.print("File: ");
         Serial.println(file.name());
         file = root.openNextFile();
     }
-
     // Serve files from LittleFS instead of SPIFFS
     server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
     server.serveStatic("/static/", LittleFS, "/");
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(LittleFS, "/index.html", "text/html");
-    });
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(LittleFS, "/index.html", "text/html"); });
 
-    server.onNotFound([](AsyncWebServerRequest *request){
-        request->send(LittleFS, "/index.html", "text/html");
-    });
+    server.onNotFound([](AsyncWebServerRequest *request)
+                      { request->send(LittleFS, "/index.html", "text/html"); });
 
     server.begin();
     Serial.println("Server started!");
